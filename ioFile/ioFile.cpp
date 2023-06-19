1a51a178026246b7692f5804da4d4c3d3684cbe4
rauda08 <raudajnnh08@gmail.com>
2023-06-19 10:33:43 +0700

    menutup file yang sudah dibuat

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -19,6 +19,9 @@ int main() {
 		getline(cin, baris);
 		//loop akan berhenti jika anda memasukkan karakter q
 		if (baris == "q") break;
-
+		//menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
 	}
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
 }
