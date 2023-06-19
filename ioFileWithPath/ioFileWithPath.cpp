3b614469fe8446dc9265d52ae3a973ae431f34be
rauda08 <raudajnnh08@gmail.com>
2023-06-19 11:13:15 +0700

    mengemblikan nilai 0

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -20,5 +20,36 @@ int main() {
 
 	//unlimited loop untuk menulis
 	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memaasukkan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukkn nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
 	}
+	//selesai dalam menulis sekarang tutup file nya
+	outfile.close();
+
+	//membuka file dalam mode embaca
+	ifstream infile;
+	//menunjukkan ke sebuah file
+	infile.open(NamaFile, ios::in);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini
+			cout << baris << 'n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "unable to open file";
+	return 0;
 }
