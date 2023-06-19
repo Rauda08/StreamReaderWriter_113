ec006b79410ea657fb07f0739e949afb9a81c20b
rauda08 <raudajnnh08@gmail.com>
2023-06-19 11:35:38 +0700

    memperbaiki kesalahan

--- exeptionStandar/exeptionStandar.cpp
+++ exeptionStandar/exeptionStandar.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-#include <exeeption>
+#include <exception>
 //untuk obyek exeption yang akan digunakan
 #include <array>
 //untuk obyek array yang akan kita gunakan
 using namespace std;
-|
+
 int main()
 {
-    cout << "Awal Program" << endl;  //penanda 1:
+    cout << "Awal Program"<<endl;  //penanda 1:
     try {
-        array<int, 3> data = { 1,2,3 };
+        array<int, 3> data = {1,2,3};
         //pesan array integer 3 elemen
         //cout<<data.at(5)<<endl;
         //memanggil array elemen ke 5
     }
-    catch (exeption& e) {
+    catch(exception& e) {
         //penangkap menggunakan obyek exeption
         cout << e.what() << endl;
         /*akan dieksekusi karena array data hanya
