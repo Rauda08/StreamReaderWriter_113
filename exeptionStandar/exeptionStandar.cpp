65158d2182555f72a400acd1f626f4b105ed768b
rauda08 <raudajnnh08@gmail.com>
2023-06-19 11:31:23 +0700

    menampilkan data baris program

--- exeptionStandar/exeptionStandar.cpp
+++ exeptionStandar/exeptionStandar.cpp
@@ -9,4 +9,15 @@ int main()
 {
     cout << "Awal Program" << endl;  //penanda 1:
     try {
-        array<int, 3> data = {1,2,3};
+        array<int, 3> data = { 1,2,3 };
+        //pesan array integer 3 elemen
+        //cout<<data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exeption& e) {
+        //penangkap menggunakan obyek exeption
+        cout << e.what() << endl;
+        /*akan dieksekusi karena array data hanya
+        memiliki 3 elemen*/
+    }
+    cout << "Baris Program yang terakhir" << endl;
